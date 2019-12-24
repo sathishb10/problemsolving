d7fe937dc77ba6753564c91cef927fc2b25601e9
sathishb10 <46063624+sathishb10@users.noreply.github.com>
2019-12-24 23:39:18 +0530

    Add files via upload

--- starshape.js
+++ starshape.js
@@ -47,3 +47,31 @@ return arr;
 }
 starshape(7)
 
+
+function starshape(n){
+    var arr=[]
+    for(var i=n;i>0;i--){
+      str=""
+      for(var j=0;j<i;j++){
+       str=str+("*")
+      }  
+      for(let k=n;k>i;k--){
+        str=str+("  ")
+      }
+      for(var j=i;j>0;j--){
+       str=str+("*")
+      }  
+    arr.push(str)
+  
+    }
+  
+   var arr2=[...arr]
+  arr[7] = arr[6]
+  arr[8] = arr[5]
+  arr[9] = arr[4]
+  arr[10] = arr[3]
+  arr[11]=arr[2]
+  arr[12]=arr[1]
+  arr[13]=arr[0]
+  
+   return arr;
