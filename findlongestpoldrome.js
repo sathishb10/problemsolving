e0b8b0acce9a511826131434985a6539587fcba3
sathishb10 <46063624+sathishb10@users.noreply.github.com>
2019-12-28 00:29:52 +0530

    Add files via upload

---
+++ findlongestpoldrome.js
@@ -0,0 +1,28 @@
+function findLongestPalindrome(sentence) {
+    // split sentence into words
+    // iterate words and collect the palindromes
+    // sort the list of palindromes by word length
+    // return the largest item in the sorted list
+    return sentence.split(' ').filter(isPalindrome)
+    .sort(sortAscendingByLength)
+    .pop()
+  }
+  
+  function reverseString(string) {
+    return string.split('').reverse().join('')
+  }
+  function isPalindrome(word) {
+    // hint: you can detect palindromes by comparing a string to its reverse
+    word = word.toLowerCase()
+    return word === reverseString(word)
+  }
+  
+  function sortAscendingByLength(a, b) {
+    if (a.length > b.length) {
+      return 1;
+    } else if (a.length < b.length) {
+      return -1;
+    }
+    return 0;
+  }
+  console.log(findLongestPalindrome('Our racecar duty is to kayak'))
